dfe76eff962718768548b2f0139c3a04afe2b647
Evel-Devel <zhenka_nikitin@mail.ru>
2020-07-23 17:40:37 +0700

    initial xcode project creation

---
+++ Youtube-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube-App
+//
+//  Created by Евгений Никитин on 23.07.2020.
+//  Copyright © 2020 Evel-Devel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
